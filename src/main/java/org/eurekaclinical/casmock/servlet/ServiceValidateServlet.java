8c3a3741b01407a5eb5c9a9581b2dddb1295bad7
Andrew Post <arpost@emory.edu>
2016-07-07 01:10:37 -0400

    Initial commit.

---
+++ src/main/java/org/eurekaclinical/casmock/servlet/ServiceValidateServlet.java
@@ -0,0 +1,50 @@
+package org.eurekaclinical.casmock.servlet;
+
+/*-
+ * #%L
+ * CAS Mock
+ * %%
+ * Copyright (C) 2016 Emory University
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.arp.javautil.io.IOUtil;
+
+/**
+ *
+ * @author Andrew Post
+ */
+public class ServiceValidateServlet extends HttpServlet {
+    private String response;
+
+    @Override
+    public void init() throws ServletException {
+        try {
+            this.response = IOUtil.readResourceAsString(getClass(), "/xml/serviceResponse.xml");
+        } catch (IOException ex) {
+            throw new AssertionError(ex);
+        }
+    }
+    
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().write(this.response);
+    }
+}
